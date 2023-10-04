192554dce294672c8746a37beb88561effa9a2aa
Alluisio Silva <alluisio.silva@ipdec.org>
2023-10-04 15:12:52 -0400

    feat: implementando listagem de bolsistas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BolsistasPage from '../views/Bolsista/BolsistasPage.vue'
+import DetalhesBolsista from '../views/Bolsista/DetalhesBolsista.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +14,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/listar-bolsistas',
+      name: 'bolsista',
+      component: BolsistasPage
+    },
+    {
+      path: '/listar-bolsistas/:id',
+      name: 'bolsista-detalhes',
+      component: DetalhesBolsista
     }
   ]
 })
